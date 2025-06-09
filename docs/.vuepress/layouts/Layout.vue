03d342a2fc216c313b3f43be2b23cafc4712fda8
Leesa Ward <leesa@doubleedesign.com.au>
2025-06-09 21:51:31 +1000

    Only show word count and read time in local dev site

--- docs/.vuepress/layouts/Layout.vue
+++ docs/.vuepress/layouts/Layout.vue
@@ -17,6 +17,8 @@ let isExample = false;
 let isToolingStudy = false;
 let shouldShowReadingData = true;
 
+const isDevSite = window.location.hostname === 'localhost';
+
 // Watch for route changes to ensure data updates
 watch(() => route.path, () => {
 	// Reset flags
@@ -56,7 +58,7 @@ watch(() => route.path, () => {
 					<Badge v-else-if="page.frontmatter.status === 'planned'" type="note" vertical="middle" :text="page.frontmatter.status" />
 					<Badge v-else type="tip" vertical="middle" text="Published" />
 				</p>
-				<div v-if="shouldShowReadingData && readingTimeLocale" class="vp-meta-item reading-time">
+				<div v-if="isDevSite && shouldShowReadingData && readingTimeLocale" class="vp-meta-item reading-time">
 					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Pro 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2025 Fonticons, Inc.--><path class="fa-secondary" opacity=".4" d="M48 224a176 176 0 1 0 352 0A176 176 0 1 0 48 224zM200 120c0-13.3 10.7-24 24-24s24 10.7 24 24l0 94.1 41 41c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-48-48c-4.5-4.5-7-10.6-7-17l0-104z"/><path class="fa-primary" d="M400 224A176 176 0 1 0 48 224a176 176 0 1 0 352 0zM224 0C347.7 0 448 100.3 448 224l0 240c0 26.5-21.5 48-48 48L48 512c-26.5 0-48-21.5-48-48L0 224C0 100.3 100.3 0 224 0zm0 96c13.3 0 24 10.7 24 24l0 94.1 41 41c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-48-48c-4.5-4.5-7-10.6-7-17l0-104c0-13.3 10.7-24 24-24z"/></svg>
 					<p aria-label="Reading time estimate:">
 						<span>{{ readingData?.time }} ({{ readingData?.words }} words).</span>
